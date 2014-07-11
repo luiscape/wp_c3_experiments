a377d66951712770ba4c4bdc318c2b80195e4afe
Luis Capelo <luiscape@gmail.com>
2014-07-11 09:37:43 -0400

    initial commit

---
+++ data/test.js
@@ -0,0 +1,37 @@
+var data = {
+  nodes: {
+    dat: {
+      'type': 'dataset',
+      'label': 'Dataset'
+    },
+    org: {
+      'type': 'org',
+      'label': 'Organizition'
+    },
+    mem1: {
+      'type': 'creator',
+      'label': 'Creator'
+    },
+    mem2: {
+      'type': 'follower',
+      'label': 'Follower'
+    },
+    mem3: {
+      'type': 'follower',
+      'label': 'Follower'
+    },
+    mem4: {
+      'type': 'follower',
+      'label': 'Follower'
+    }
+  },
+  edges: {
+    dat: {
+      org: {},
+      mem1: {},
+      mem2: {},
+      mem3: {},
+      mem4: {}
+    }
+  }
+};
